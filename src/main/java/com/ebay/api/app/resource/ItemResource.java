c26133abbfe42b2f57bbe32240a8c8400d9fab83
trifon <trifont@gmail.com>
2020-09-08 08:46:32 +0300

    Minor code formatting change.

--- src/main/java/com/ebay/api/app/resource/ItemResource.java
+++ src/main/java/com/ebay/api/app/resource/ItemResource.java
@@ -24,36 +24,36 @@ import com.ebay.soap.eBLBaseComponents.ItemType;
 @Path("/item")
 public class ItemResource {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(AuthResource.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthResource.class);
 
-  private static final DetailLevelCodeType[] RETURN_ALL_ONLY = new DetailLevelCodeType[] { DetailLevelCodeType.RETURN_ALL };
+	private static final DetailLevelCodeType[] RETURN_ALL_ONLY = new DetailLevelCodeType[] {
+			DetailLevelCodeType.RETURN_ALL };
 
-  @Context
-  private HttpServletRequest req;
+	@Context
+	private HttpServletRequest req;
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  @Path("/{itemId}")
-  public Response getItem(@PathParam("itemId") String itemId) {
-    final String token;
-    if (StringUtils.isBlank(token = HttpRequestUtil.getOAuthToken(req))) {
-      final Message msg = new Message("An 'Authorization: Bearer <userToken>' header must be present in the request.");
-      return Response.status(Status.UNAUTHORIZED).entity(msg).build();
-    } else if (StringUtils.isBlank(itemId)) {
-      final Message msg = new Message("Invalid item id.");
-      return Response.status(Status.BAD_REQUEST).entity(msg).build();
-    } else {
-      try {
-        final GetItemCall gc = new GetItemCall(new OAuthApiContext(Environment.PRODUCTION, token));
-        gc.setDetailLevel(RETURN_ALL_ONLY);
-        final ItemType item = gc.getItem(itemId);
-        return Response.ok().entity(item).build();
-      } catch (Exception unexpectedEx) {
-        LOGGER.error("Failed to fetch details for item {}", itemId, unexpectedEx);
-        final Message msg = new Message(unexpectedEx.getMessage());
-        return Response.serverError().entity(msg).build();
-      }
-    }
-
-  }
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{itemId}")
+	public Response getItem(@PathParam("itemId") String itemId) {
+		final String token;
+		if (StringUtils.isBlank(token = HttpRequestUtil.getOAuthToken(req))) {
+			final Message msg = new Message("An 'Authorization: Bearer <userToken>' header must be present in the request.");
+			return Response.status(Status.UNAUTHORIZED).entity(msg).build();
+		} else if (StringUtils.isBlank(itemId)) {
+			final Message msg = new Message("Invalid item id.");
+			return Response.status(Status.BAD_REQUEST).entity(msg).build();
+		} else {
+			try {
+				final GetItemCall gc = new GetItemCall(new OAuthApiContext(Environment.PRODUCTION, token));
+				gc.setDetailLevel(RETURN_ALL_ONLY);
+				final ItemType item = gc.getItem(itemId);
+				return Response.ok().entity(item).build();
+			} catch (Exception unexpectedEx) {
+				LOGGER.error("Failed to fetch details for item {}", itemId, unexpectedEx);
+				final Message msg = new Message(unexpectedEx.getMessage());
+				return Response.serverError().entity(msg).build();
+			}
+		}
+	}
 }
